5f360fc7ed9c4f408c9b85765238f2b4f804ea0b
Mukovhe Netshivhale <terrynets.m@gmail.com>
2026-03-25 10:20:51 +0200

    Add files via upload

---
+++ Sub3.py
@@ -0,0 +1,11 @@
+#leap year
+
+year=int(input())
+
+if year%100==0 and year%400==0:
+    print(year,'is a leap year')
+elif year%100!=0 and year%4==0:
+    print(year,'is a leap year')
+else:
+    print(year,'is not a leap year')
+
